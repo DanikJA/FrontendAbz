33217e079cceaaac055ef444f2c41d616fade65b
Danik2023 <danik.kyrteu2@gmail.com>
2025-07-31 20:52:14 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header";
+import Hero from "./Hero";
+import UsersSection from "./Users";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Hero />
+      <UsersSection />
+    </div>
+  );
+}
+
+export default App;
